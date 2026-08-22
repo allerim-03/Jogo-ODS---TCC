b1860df6f2cd2b75f0685756c27bc8534ac8e7ab
nicolyramalho <contato.niramalho@gmail.com>
2026-08-22 17:34:16 -0300

    SPRITES BONITINHOS

--- js/jogo1/player.js
+++ js/jogo1/player.js
@@ -1,63 +1,94 @@
-// Objeto do Jogador com Física de Platformer
+// Carregamento dos sprites de Andando (32x32)
+const spritesWalk = [];
+for (let i = 1; i <= 4; i++) {
+    const img = new Image();
+    img.src = `img/gota-walk-${i}.png`;
+    spritesWalk.push(img);
+}
+
 const jogadorJogo1 = {
     x: 100,
     y: 200,
-    raio: 20,
+    largura: 32,
+    altura: 32,
     velocidadeX: 5,
     velocidadeY: 0,
     forcaPulo: -12,
     gravidade: 0.6,
     noChao: false,
-    cor: "#3498db",
+    
+    // Controle de Animação
+    frameAtual: 0,
+    contadorTempo: 0,
+    velocidadeAnimacao: 8,
+    olhandoDireita: true,
 
     atualizar: function(alturaChao) {
-        // Movimentação Horizontal (A / D ou Setas)
+        let movendo = false;
+
+        // Movimentação
         if (controlesJogo1.esquerda) {
             this.x -= this.velocidadeX;
+            this.olhandoDireita = false;
+            movendo = true;
         }
         if (controlesJogo1.direita) {
             this.x += this.velocidadeX;
+            this.olhandoDireita = true;
+            movendo = true;
         }
 
-        // Pulo (Espaço, W ou Seta para Cima)
+        // Pulo
         if ((controlesJogo1.cima || controlesJogo1.espaco) && this.noChao) {
             this.velocidadeY = this.forcaPulo;
             this.noChao = false;
         }
 
-        // Aplicação da Gravidade
+        // Gravidade e Colisão com Chão
         this.velocidadeY += this.gravidade;
         this.y += this.velocidadeY;
 
-        // Colisão com o Chão
-        if (this.y + this.raio >= alturaChao) {
-            this.y = alturaChao - this.raio;
+        if (this.y + this.altura >= alturaChao) {
+            this.y = alturaChao - this.altura;
             this.velocidadeY = 0;
             this.noChao = true;
         }
 
-        // Limite para não voltar além do início do mapa (borda esquerda)
-        if (this.x - this.raio < 0) {
-            this.x = this.raio;
+        if (this.x < 0) this.x = 0;
+
+        // Animação roda quando está se movendo (ou trava no frame 0 ao parar)
+        if (movendo) {
+            this.contadorTempo++;
+            if (this.contadorTempo >= this.velocidadeAnimacao) {
+                this.contadorTempo = 0;
+                this.frameAtual = (this.frameAtual + 1) % 4;
+            }
+        } else {
+            this.frameAtual = 0; // Frame parado padrão
+            this.contadorTempo = 0;
         }
     },
 
     desenhar: function(ctx, cameraX) {
-        // Desenha o jogador aplicando o deslocamento da câmera
+        ctx.imageSmoothingEnabled = false;
+
+        const imgAtual = spritesWalk[this.frameAtual];
         const posX = this.x - cameraX;
 
-        // Corpo da Gota
-        ctx.fillStyle = this.cor;
-        ctx.beginPath();
-        ctx.arc(posX, this.y, this.raio, 0, Math.PI * 2);
-        ctx.fill();
-        ctx.closePath();
+        ctx.save();
 
-        // Brilho/Reflexo
-        ctx.fillStyle = "#ffffff";
-        ctx.beginPath();
-        ctx.arc(posX - 6, this.y - 6, 5, 0, Math.PI * 2);
-        ctx.fill();
-        ctx.closePath();
+        if (!this.olhandoDireita) {
+            ctx.translate(posX + this.largura, this.y);
+            ctx.scale(-1, 1);
+            if (imgAtual.complete && imgAtual.naturalWidth !== 0) {
+                ctx.drawImage(imgAtual, 0, 0, this.largura, this.altura);
+            }
+        } else {
+            if (imgAtual.complete && imgAtual.naturalWidth !== 0) {
+                ctx.drawImage(imgAtual, posX, this.y, this.largura, this.altura);
+            }
+        }
+
+        ctx.restore();
     }
 };
